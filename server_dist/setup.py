6394d0a9f64342ac9a37445db92054cbc529e017
anton <rkzton@yandex.ru>
2024-10-26 12:32:26 +0300

    lesson_16 (Path corrected - delete __file__)

--- server_dist/setup.py
+++ server_dist/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="messenger_123_server",
-      version="0.0.1",
+      version="0.0.2",
       description="messenger_123_server",
       author="Semushkin Anton",
       author_email="rkzton@yandex.ru",
